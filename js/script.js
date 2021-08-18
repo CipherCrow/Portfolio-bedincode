3a859c6148f063b125de2b48e4b1229ec2f27161
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2021-08-18 00:40:30 -0300

    :up: Sessão de habilidades

--- js/script.js
+++ js/script.js
@@ -21,3 +21,20 @@ function linkAction(){
     navMenu.classList.remove('mostrar-menu');
 }
 navlink.forEach(e=>{e.addEventListener('click',linkAction)});
+
+/* Habilidades */
+const habilidadesContent = document.getElementsByClassName("habilidades__content"),
+      habilidadesHeader =  document.querySelectorAll("habilidades__header");
+
+function toggleHabilidade(){
+    let itemClass = this.parentNode.className;
+    for( i = 0;i < habilidadesContent.length;i++ ){
+        habilidadesContent[i].className = 'habilidades__content habilidades__fechado';
+    }
+    if(itemClass === 'habilidades__content habilidades__fechado'){
+        this.parentNode.className = 'habilidades__content habilidades__aberto'
+    }
+}
+habilidadesHeader.forEach((el) =>{
+    el.addEventListener('click',toggleHabilidade);
+})
