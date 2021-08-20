03f99996b494c4532a7da29066b49f3f7de4b9c7
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2021-08-20 18:47:37 -0300

    :up: portfolio
    Usando api swiper para fazer a galeria

--- js/script.js
+++ js/script.js
@@ -80,3 +80,17 @@ modalCloses.forEach((modalClose)=>{
         })
     })
 })
+
+/* swiper portfolio*/
+let swiper = new Swiper('.portfolio__container',{
+    cssMode: true,
+    loop: true,
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
